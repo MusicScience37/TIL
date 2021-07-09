deeba656fd9570df2461802b95baae355da6a7eb
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-07-10 00:38:51 +0900

    画像を変更

--- source/conf.py
+++ source/conf.py
@@ -66,7 +66,7 @@ if sys.platform == 'win32':
 extensions += ['sphinxext.opengraph']
 ogp_site_url = 'https://til.musicscience37.com/'
 ogp_site_name = "MusicScience37's TIL"
-ogp_image = 'https://til.musicscience37.com/_static/KIcon80white.png'
+ogp_image = 'https://til.musicscience37.com/_static/KIcon512white.png'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -96,7 +96,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'KIcon/KIcon512white.png']
 
 html_title = 'MusicScience37\'s TIL'
 
