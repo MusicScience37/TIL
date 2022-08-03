9004c1aeca5c1a1ac8ced7329516cd6b69bca104
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-08-03 13:10:33 +0000

    pre-commit の設定の更新

--- source/development/cpp/serialization/serialization/result_20210205_Ubuntu/convert_xml_to_csv.py
+++ source/development/cpp/serialization/serialization/result_20210205_Ubuntu/convert_xml_to_csv.py
@@ -37,13 +37,15 @@ def parse_bench_in_xml(bench_result: ET.Element) -> Catch2BenchResult:
         Catch2BenchResult: parsed data
     """
 
-    name = bench_result.attrib['name']
-    mean_elem = bench_result.find('mean')
-    mean = mean_elem.attrib['value']
-    lower_bound = mean_elem.attrib['lowerBound']
-    upper_bound = mean_elem.attrib['upperBound']
+    name = bench_result.attrib["name"]
+    mean_elem = bench_result.find("mean")
+    mean = mean_elem.attrib["value"]
+    lower_bound = mean_elem.attrib["lowerBound"]
+    upper_bound = mean_elem.attrib["upperBound"]
 
-    return Catch2BenchResult(name=name, mean=mean, lower_bound=lower_bound, upper_bound=upper_bound)
+    return Catch2BenchResult(
+        name=name, mean=mean, lower_bound=lower_bound, upper_bound=upper_bound
+    )
 
 
 def parse_xml(filepath: str) -> pd.DataFrame:
@@ -60,9 +62,9 @@ def parse_xml(filepath: str) -> pd.DataFrame:
     root = tree.getroot()
 
     bench_results = []
-    for group in root.findall('Group'):
-        for test_case in group.findall('TestCase'):
-            for bench_result in test_case.findall('BenchmarkResults'):
+    for group in root.findall("Group"):
+        for test_case in group.findall("TestCase"):
+            for bench_result in test_case.findall("BenchmarkResults"):
                 bench_results.append(parse_bench_in_xml(bench_result))
 
     return pd.DataFrame(
@@ -70,7 +72,7 @@ def parse_xml(filepath: str) -> pd.DataFrame:
             (result.name, result.mean, result.lower_bound, result.upper_bound)
             for result in bench_results
         ],
-        columns=('name', 'mean_ns', 'lower_bound_ns', 'upper_bound_ns')
+        columns=("name", "mean_ns", "lower_bound_ns", "upper_bound_ns"),
     )
 
 
@@ -81,11 +83,12 @@ def parse_bench_string() -> pd.DataFrame:
         pd.DataFrame: results
     """
 
-    xml_results = parse_xml(DATA_DIR + '/bench_string.xml')
-    parsed_name = xml_results['name'].str.extract(
-        R'(?P<procedure>[a-z]*) string\((?P<data_size>\d*)\) with (?P<library>.*)')
+    xml_results = parse_xml(DATA_DIR + "/bench_string.xml")
+    parsed_name = xml_results["name"].str.extract(
+        R"(?P<procedure>[a-z]*) string\((?P<data_size>\d*)\) with (?P<library>.*)"
+    )
     bench_results = pd.concat([parsed_name, xml_results], axis=1)
-    bench_results['data_type'] = 'string'
+    bench_results["data_type"] = "string"
     return bench_results
 
 
@@ -96,11 +99,12 @@ def parse_bench_double() -> pd.DataFrame:
         pd.DataFrame: results
     """
 
-    xml_results = parse_xml(DATA_DIR + '/bench_double.xml')
-    parsed_name = xml_results['name'].str.extract(
-        R'(?P<procedure>[a-z]*) double\[(?P<data_size>\d*)\] with (?P<library>.*)')
+    xml_results = parse_xml(DATA_DIR + "/bench_double.xml")
+    parsed_name = xml_results["name"].str.extract(
+        R"(?P<procedure>[a-z]*) double\[(?P<data_size>\d*)\] with (?P<library>.*)"
+    )
     bench_results = pd.concat([parsed_name, xml_results], axis=1)
-    bench_results['data_type'] = 'double'
+    bench_results["data_type"] = "double"
     return bench_results
 
 
@@ -111,28 +115,28 @@ def parse_bench_struct() -> pd.DataFrame:
         pd.DataFrame: results
     """
 
-    xml_results = parse_xml(DATA_DIR + '/bench_struct.xml')
-    parsed_name = xml_results['name'].str.extract(
-        R'(?P<procedure>[a-z]*) struct with (?P<library>.*)')
+    xml_results = parse_xml(DATA_DIR + "/bench_struct.xml")
+    parsed_name = xml_results["name"].str.extract(
+        R"(?P<procedure>[a-z]*) struct with (?P<library>.*)"
+    )
     bench_results = pd.concat([parsed_name, xml_results], axis=1)
-    bench_results['data_type'] = 'struct'
-    bench_results['data_size'] = 1
+    bench_results["data_type"] = "struct"
+    bench_results["data_size"] = 1
     return bench_results
 
 
 def main():
-    """Main function
-    """
+    """Main function"""
 
     string_results = parse_bench_string()
     double_results = parse_bench_double()
     struct_results = parse_bench_struct()
     benchmarks = pd.concat(
-        [string_results, double_results, struct_results],
-        axis=0, ignore_index=True)
+        [string_results, double_results, struct_results], axis=0, ignore_index=True
+    )
     print(benchmarks)
-    benchmarks.to_csv(DATA_DIR + '/bench.csv', index=False)
+    benchmarks.to_csv(DATA_DIR + "/bench.csv", index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
