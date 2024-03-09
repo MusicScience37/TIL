170ecf2edb89319f6a29a3af3d49d2edc6f92b64
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-03-09 15:04:33 +0000

    ビルドスクリプトを古いファイルから変更

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-base_dir=$(dirname $0)
+set -eu
+
+cd $(dirname $0)
 mkdir -p build
 if [ ! -f build/favicon.ico ]; then
     wget -nv https://kicon.musicscience37.com/KIcon.ico -O build/favicon.ico
 fi
-PYDEVD_DISABLE_FILE_VALIDATION=1 sphinx-autobuild $base_dir/source $base_dir/build --host 0 --port 3737
+PYDEVD_DISABLE_FILE_VALIDATION=1 sphinx-build -M html source build
